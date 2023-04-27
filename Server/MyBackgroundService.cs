b0f4012a04f9861eb7f12c742e1e3ae884096f13
comrade-napoleon <44015502+comrade-napoleon@users.noreply.github.com>
2023-04-27 15:29:55 +0200

    1. commit

---
+++ Server/MyBackgroundService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WWWatering;
+public class MyBackgroundService : IHostedService, IDisposable
+{
+    private readonly MyService _myService;
+    private Timer _timer;
+
+    public MyBackgroundService(MyService myService)
+    {
+        _myService = myService;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5)); // Adjust the interval as needed
+        return Task.CompletedTask;
+    }
+
+    private void DoWork(object? state)
+    {
+        _myService.GetMessage();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _timer?.Change(Timeout.Infinite, 0);
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
+    }
+}
